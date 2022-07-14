e2745d1fc3bf42bb44c97aa99b209460140c1f0a
Erza <95280756+Hinata816@users.noreply.github.com>
2022-07-14 22:00:19 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,23 +23,23 @@ global.language = english  //change indonesia to english if you don't understand
 
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
+global.botname = "Hina-Botོ" //namabot kalian
+global.ownername= "ᴹᴿS᭄ Jessi ×፝֟͜×" //nama kalian
+global.myweb ="https://instagram.com/_lucrza?igshid=ZDc0MTNkNmI=" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UC9Fr0Yi2-lH_S5JwKqwkfDA" //bebas asal jan hapus
+global.github = "https://github.com/Hinata816" //bebas
+global.email = "hinata1232011@gmail.com" //bebas
+global.region = "Palestine" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.owner = ["97259-556-1204","972595561204"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "97259-556-1204" // nomor wa kalian
+global.ownernomerr = "+97259-556-1204" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Hina-Botོ' //sticker wm ubah
+global.author = 'Jessi' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
